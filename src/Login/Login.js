e17c642b838bb34b91c78a233a4ca84a38bcefa8
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2020-12-24 16:36:12 +0900

    MODIFY feature/7/CheckPoint_Write_getAccessToken_method/ Modify Login C related to changes in CheckPoint.js

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Login.css";
 import axios from "axios";
+import { Link, Switch, Route, Redirect, withRouter } from "react-router-dom";
 
 class Login extends Component {
   constructor(props) {
@@ -10,7 +11,36 @@ class Login extends Component {
       email: "",
       isEmailChecked: false,
       isPasswordChecked: false,
+      password: "",
     };
+
+    this.naverLoginHandler = this.naverLoginHandler.bind(this);
+    this.googleLoginHandler = this.googleLoginHandler.bind(this);
+
+    // OAuth 인증이 완료되면 authorization code와 함께 callback url로 리디렉션 합니다.
+    // 참고: https://docs.github.com/en/free-pro-team@latest/developers/apps/identifying-and-authorizing-users-for-github-apps
+
+    this.NAVER_LOGIN_URL =
+      "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=Yn5blabiliLbi8Ed8Je4&state=waftest&redirect_uri=http://localhost:3000/";
+
+    this.GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=218828135580-63brp05lohg6jb7f58rgjhueorgtv9d6.apps.googleusercontent.com&redirect_uri=http://localhost:3000&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile`;
+  }
+
+  // let GOOGLE_LOGIN_URL = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+  // let params = {
+  //   scope: 'https%3A//www.googleapis.com/auth/drive.metadata.readonly',
+  //   client_id: '218828135580-63brp05lohg6jb7f58rgjhueorgtv9d6.apps.googleusercontent.com',
+  //   redirect_uri: 'http://localhost:3000',
+  //   response_type: 'code'
+  // }
+  // GOOGLE_LOGIN_URL.search = new URLSearchParams(params).toString();
+
+  naverLoginHandler() {
+    window.location.assign(this.NAVER_LOGIN_URL);
+  }
+
+  googleLoginHandler(e) {
+    window.location.assign(this.GOOGLE_LOGIN_URL);
   }
 
   EmailValidator = (e) => {
@@ -40,8 +70,36 @@ class Login extends Component {
 
   LoginHandler = (e) => {
     if (this.state.isEmailChecked && this.state.isPasswordChecked) {
-      console.log(e);
-      axios.post();
+      axios({
+        method: "POST",
+        url: "https://localhost:4000/users/signin",
+        data: {
+          email: this.state.email,
+          password: this.state.password,
+        },
+        header: { "Content-Type": "application/json", withCredentials: true },
+      })
+        .then((res) => {
+          let { username } = res.data;
+          this.props.LoginHandler(true);
+          this.props.setUserName({ username });
+        })
+        .then((res) => {
+          return axios.get("https://localhost:4000/myfridge/:userid", {
+            withCredentials: true,
+          });
+        })
+        .then((resp) => {
+          let { item, category, part, created_at, modified_at } = resp.data;
+          this.props.setUserInfo({
+            item,
+            category,
+            part,
+            created_at,
+            modified_at,
+          });
+        })
+        .catch((err) => alert(err));
     }
   };
 
@@ -55,29 +113,35 @@ class Login extends Component {
         <div className="subContainer">
           <input
             placeholder="email"
-            className="inputEmail"
+            className="input__Email"
             onChange={this.EmailValidator.bind(this)}
           ></input>
           <input
             placeholder="password"
-            className="inputPassword"
+            className="input__Password"
             type="password"
             onChange={this.PasswordValidator.bind(this)}
           ></input>
           <button
-            className="loginButton"
+            className="login__Button"
             onClick={this.LoginHandler.bind(this)}
           >
             로그인
           </button>
-          <button
-            className="singUpButton"
-            onClick={this.convertToSignup.bind(this)}
-          >
-            회원가입
-          </button>
-          <div className="naverOauth"></div>
-          <div className="googleOauth"></div>
+          <Link to="/users/signup">
+            <button
+              className="singUp__Button"
+              onClick={this.convertToSignup.bind(this)}
+            >
+              회원가입
+            </button>
+          </Link>
+          <div className="naver__Oauth" onClick={this.naverLoginHandler}></div>
+          <div
+            name="google"
+            className="google__Oauth"
+            onClick={this.googleLoginHandler}
+          ></div>
         </div>
       </div>
     );
