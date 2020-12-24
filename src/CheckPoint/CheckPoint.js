18497d489edcca379ea91e56dba78260a90898e3
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2020-12-24 16:34:45 +0900

    MODIFY feature/7/CheckPoint_Write_getAccessToken_method

--- src/CheckPoint/CheckPoint.js
+++ src/CheckPoint/CheckPoint.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Login from ".././Login/Login";
-import Myfridge from ".././Myfridge/Myfridge";
+import Myfridge from "../Myfridge/Myfridge";
+import axios from "axios";
 
 class CheckPoint extends Component {
   constructor(props) {
@@ -9,16 +10,73 @@ class CheckPoint extends Component {
     this.state = {
       isLogin: false,
       accessToken: "",
+      userName: "",
+      userData: null,
+      path: null,
     };
+
+    this.LoginHandler = this.LoginHandler.bind(this);
+    this.setUserInfo = this.setUserInfo.bind(this);
+    this.setUserName = this.setUserName.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+  }
+
+  async getAccessToken(authorizationCode) {
+    let resp = await axios.post("https://localhost:4000/callback", {
+      authorizationCode: authorizationCode,
+      path: this.state.path,
+    });
+
+    this.setState({
+      isLogin: true,
+      accessToken: resp.data.accessToken,
+    });
+  }
+
+  async naverAccessToken(authorizationCode) {
+    let resp = await axios.post("https://localhost:4000/naver", {
+      authorizationCode: authorizationCode,
+    });
+
+    this.setState({
+      isLogin: true,
+      accessToken: resp.data.accessToken,
+    });
+  }
+
+  LoginHandler = () => {
+    this.setState({ isLogin: true });
+  };
+
+  setUserInfo(object) {
+    this.setState({ userData: object });
+  }
+  setUserName(name) {
+    this.setState({ userName: name });
+  }
+
+  componentDidMount() {
+    // authorization server로부터 클라이언트로 리디렉션된 경우, authorization code가 함께 전달됩니다.
+    // ex) http://localhost:3000/?code=5e52fb85d6a1ed46a51f
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get("code");
+
+    if (authorizationCode) {
+      this.getAccessToken(authorizationCode);
+    }
   }
 
   render() {
     const { isLogin, accessToken } = this.state;
 
     return (
-      <div>
+      <div className="CheckPoint">
         {!isLogin ? (
-          <Login></Login>
+          <Login
+            LoginHandle={this.LoginHandler}
+            setUserInfo={this.setUserInfo}
+            setUserName={this.setUserName}
+          ></Login>
         ) : (
           <Myfridge accessToken={accessToken}></Myfridge>
         )}
