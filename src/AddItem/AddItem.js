0cc90f6ac52ea220208d8835f7268defc97dd4d8
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2020-12-27 18:09:59 +0900

    ADD feature/10/Myfridge_Add_Contents AddItem C is being written by kelly

---
+++ src/AddItem/AddItem.js
@@ -0,0 +1,154 @@
+import axios from "axios";
+import React, { Component } from "react";
+
+class AddItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [
+        "dairy",
+        "fruit",
+        "vegetables",
+        "meat",
+        "fish",
+        "frozen",
+        "eggs",
+      ],
+      collection: [
+        {
+          id: 1,
+          item: "brocolli",
+          category: "vegitable",
+          part: "fridge",
+          created_at: "2020-12-25",
+          modified_at: "",
+        },
+      ],
+
+      purchase: {
+        item: "",
+        category: "",
+        part: "",
+        created_at: new Date(),
+        modified_at: "",
+        boughtToday: true,
+      },
+    };
+  }
+
+  // inputValueHandler = (key) => (e) => {
+  //   this.setState({ [key]: e.target.value });
+  // };
+
+  inputValueHandler = (key) => (e) => {
+    this.setState({
+      purchase: {
+        [key]: e.target.value,
+      },
+    });
+  };
+
+  boughtToday = (e) => {
+    if (e.target.checked) {
+      this.setState({
+        purchase: {
+          boughtToday: true,
+        },
+      });
+    } else {
+      this.setState({
+        purchase: {
+          boughtToday: false,
+        },
+      });
+    }
+  };
+
+  putDatabase = () => {
+    axios
+      .post(
+        "https://localhost:4000//myfridge/cart/:id", //개인마다 카트가 다름.
+        this.state.collection,
+        {
+          "Content-Type": "application/json",
+          withCredentials: true,
+        }
+      )
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
+  putCollection = (e) => {
+    console.log(this.state);
+    console.log(e);
+  };
+
+  deleteCollection = () => {};
+
+  render() {
+    const { categories, collection } = this.state;
+    const { userName } = this.props;
+
+    return (
+      <div>
+        <div>{userName}님의 카트입니다.</div>
+        {/* 리스트업을 위한 자리 */}
+        <ul>
+          {/* eslint-disable-next-line array-callback-return */}
+          {collection.map((item) => {
+            return (
+              <li key={item.id}>
+                <div>{item.item}</div>
+                <div>{item.category}</div>
+                <div>{item.part}</div>
+                {item.modified_at ? (
+                  <div>{item.modified_at}</div>
+                ) : (
+                  <div>{item.created_at}</div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+        {/* addItem을 위한 인풋창 */}
+        <button onClick={this.deleteCollection}> - </button>
+        <select
+          name="categories"
+          type="category"
+          onChange={this.inputValueHandler("category")}
+        >
+          {categories.map((c, index) => {
+            return (
+              <option value={c} key={index}>
+                {c}
+              </option>
+            );
+          })}
+        </select>
+        <input
+          placeholder="구매하신 것을 적어주세요"
+          type="item"
+          onChange={this.inputValueHandler("item")}
+        ></input>
+        <select type="part" onChange={this.inputValueHandler("part")}>
+          <option value="fridge">냉장</option>
+          <option value="frozen">냉동</option>
+        </select>
+        <input
+          type="checkbox"
+          name="오늘구매"
+          onClick={this.boughtToday.bind(this)}
+        ></input>
+        <input
+          type="date"
+          onChange={this.inputValueHandler("modified_at")}
+        ></input>
+        <button onClick={this.putCollection}> + </button>
+        <button onClick={this.putDatabase}> 냉장고에 넣기 </button>
+      </div>
+    );
+  }
+}
+
+export default AddItem;
