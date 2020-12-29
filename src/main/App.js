e68d0279d35aa062b486082dd8550542401ad489
dawa93 <dawa93@naver.com>
2020-12-29 13:57:25 +0900

    MODIFFY App.js

--- src/main/App.js
+++ src/main/App.js
@@ -3,10 +3,10 @@ import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import Main from "./Main";
 import CheckPoint from "../CheckPoint/CheckPoint";
-import Demo from "../Demo_fridge/Demo";
 import Signup from "../Sign_up/Signup";
 import AddItem from "../AddItem/AddItem";
 import Demo from "../Demo_fridge/Demo";
+import Myfridge from "../Myfridge/Myfridge";
 
 class App extends React.Component {
   constructor(props) {
@@ -24,7 +24,7 @@ class App extends React.Component {
           <Route exact path="/demofridge" render={() => <Demo />} />
           <Route exact path="/cart" render={() => <AddItem />} />
           <Route exact path="/users" render={() => <CheckPoint />} />
-          <Route exact path="/demofridge" render={() => <Demo />} />
+          <Route exact path="/myfridge" render={() => <Myfridge />} />
           <Route exact path="/signup" render={() => <Signup />} />
         </Switch>
       </div>
