f7e414de51013b552c7b98f35ff17b5050bd3e2f
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2020-12-31 13:48:16 +0900

    MODIFY dev Signup more detailed

--- src/Sign_up/Signup.js
+++ src/Sign_up/Signup.js
@@ -19,25 +19,44 @@ class Signup extends Component {
   };
 
   clickBtn = () => {
-    console.log(this.state.email);
-    console.log(this.state.password);
-    console.log(this.state.username);
-    console.log(this.state.mobile);
-    axios
-      .post("http://localhost:4000/users/signup", this.state)
-      .then(() => this.props.history.push("/users"));
+    const { email, password, mobile } = this.state;
+    // console.log(this.state.email);
+    // console.log(this.state.password);
+    // console.log(this.state.username);
+    // console.log(this.state.mobile);
+    if (
+      password.length >= 8 &&
+      mobile[0] + mobile[1] + mobile[2] === "010" &&
+      mobile.length === 11 &&
+      email.includes("@") &&
+      email.includes(".")
+    ) {
+      axios
+        .post("http://localhost:4000/users/signup", this.state)
+        .then((res) => {
+          console.log(res);
+          this.props.history.push("/users");
+        })
+        .catch((err) => {
+          if (err) {
+            alert("이미 동일한 이메일이 존재합니다");
+          }
+        });
+    } else {
+      alert("유효한 정보가 아닙니다. 다시 작성해주세요");
+    }
   };
 
   render() {
     return (
       <div id="signup">
         <input
-          placeholder="이름을 작성해주세요"
+          placeholder="홍길동"
           type="username"
           onChange={this.handleInputValue("username")}
         ></input>
         <input
-          placeholder="비밀번호를 작성해주세요"
+          placeholder="영문 및 숫자 8자리"
           type="password"
           onChange={this.handleInputValue("password")}
         ></input>
@@ -47,7 +66,7 @@ class Signup extends Component {
           onChange={this.handleInputValue("email")}
         ></input>
         <input
-          placeholder="모바일 넘버를 작성해주세요."
+          placeholder="01012345678"
           type="mobile"
           onChange={this.handleInputValue("mobile")}
         ></input>
