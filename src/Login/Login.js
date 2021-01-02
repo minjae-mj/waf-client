f82d3d4707a474d9e6a0b8cfb5b79cd51ef94969
minjae-mj <65207835+minjae-mj@users.noreply.github.com>
2021-01-03 01:13:02 +0900

    feature/css/minjae/signin_signup

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Login.css";
-import { Link, withRouter } from "react-router-dom";
+import btn_google from "./btn_google.png";
+import btn_naver from "./btn_naver.png";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 class Login extends React.Component {
@@ -17,23 +19,11 @@ class Login extends React.Component {
     this.naverLoginHandler = this.naverLoginHandler.bind(this);
     this.googleLoginHandler = this.googleLoginHandler.bind(this);
 
-    // OAuth 인증이 완료되면 authorization code와 함께 callback url로 리디렉션 합니다.
-    // 참고: https://docs.github.com/en/free-pro-team@latest/developers/apps/identifying-and-authorizing-users-for-github-apps
-
     this.NAVER_LOGIN_URL =
       "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=Yn5blabiliLbi8Ed8Je4&state=waftest&redirect_uri=http://localhost:3000/users";
 
     this.GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=218828135580-63brp05lohg6jb7f58rgjhueorgtv9d6.apps.googleusercontent.com&redirect_uri=http://localhost:3000/users&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile`;
   }
-
-  // let GOOGLE_LOGIN_URL = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-  // let params = {
-  //   scope: 'https%3A//www.googleapis.com/auth/drive.metadata.readonly',
-  //   client_id: '218828135580-63brp05lohg6jb7f58rgjhueorgtv9d6.apps.googleusercontent.com',
-  //   redirect_uri: 'http://localhost:3000',
-  //   response_type: 'code'
-  // }
-  // GOOGLE_LOGIN_URL.search = new URLSearchParams(params).toString();
 
   naverLoginHandler() {
     window.location.assign(this.NAVER_LOGIN_URL);
@@ -69,7 +59,7 @@ class Login extends React.Component {
     }
   };
 
-  LoginHandler = (e) => {
+  LoginHandler = () => {
     if (this.state.isEmailChecked && this.state.isPasswordChecked) {
       axios({
         method: "POST",
@@ -84,24 +74,9 @@ class Login extends React.Component {
           console.log(res);
           this.props.LoginHandler(res.data.username, res.data.userid, true);
         })
-        .then((res) => {
+        .then(() => {
           console.log("done");
         })
-        // .then((res) => {
-        //   return axios.get("https://localhost:4000/myfridge/:userid", {
-        //     withCredentials: true,
-        //   });
-        // })
-        // .then((resp) => {
-        //   let { item, category, part, created_at, modified_at } = resp.data;
-        //   this.props.setUserInfo({
-        //     item,
-        //     category,
-        //     part,
-        //     created_at,
-        //     modified_at,
-        //   });
-        // })
         .catch((err) => console.log(err));
     }
   };
@@ -112,40 +87,46 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div id="main">
-        <div className="subContainer">
-          <input
-            placeholder="email"
-            className="input__Email"
-            onChange={this.EmailValidator.bind(this)}
-          ></input>
-          <input
-            placeholder="password"
-            className="input__Password"
-            type="password"
-            onChange={this.PasswordValidator.bind(this)}
-          ></input>
-          <button
-            className="login__Button"
-            onClick={this.LoginHandler.bind(this)}
-          >
-            로그인
-          </button>
-          {/* <Link to="/users/signup"> */}
-          <button
-            className="singUp__Button"
-            onClick={this.convertToSignup.bind(this)}
-          >
-            회원가입
-          </button>
-          {/* </Link> */}
-          <div className="naver__Oauth" onClick={this.naverLoginHandler}></div>
-          <div
-            name="google"
-            className="google__Oauth"
+      <div class="register__wrapper">
+        <input
+          placeholder="이메일"
+          className="input__email"
+          onChange={this.EmailValidator.bind(this)}
+        />
+        <input
+          placeholder="비밀번호"
+          className="input__password"
+          type="password"
+          onChange={this.PasswordValidator.bind(this)}
+        />
+
+        <div id="oauth__box">
+          <img
+            src={btn_google}
+            alt="google__signin"
+            className="btn__google"
             onClick={this.googleLoginHandler}
-          ></div>
+          />
+          <img
+            src={btn_naver}
+            alt="naver__signin"
+            className="btn__naver"
+            onClick={this.naverLoginHandler}
+          />
         </div>
+
+        <button
+          className="btn__register"
+          onClick={this.LoginHandler.bind(this)}
+        >
+          로그인
+        </button>
+        <button
+          className="btn__signup"
+          onClick={this.convertToSignup.bind(this)}
+        >
+          회원가입
+        </button>
       </div>
     );
   }
