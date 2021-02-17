065fb6f3ec5e43934c5db01a8617cc066095faf8
Minjae Kwon <minjaekwon@Minjaes-iMac.local>
2021-02-17 18:14:32 +0900

    Refactor responsive css on Demo page

--- src/Demo_fridge/Demo.js
+++ src/Demo_fridge/Demo.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './Demo.css';
-import logo from './Waf.png';
-import AddItems from './AddItems';
-import left_fridge from './img_fridge/left_fridge.png';
-import mandu from './img_fridge/mandu.png';
-import eggs from './img_fridge/eggs.png';
-import dairy from './img_fridge/dairy.png';
-import right_fridge from './img_fridge/right_fridge.png';
-import fish from './img_fridge/fish.png';
-import meat from './img_fridge/meat.png';
-import veges from './img_fridge/veges.png';
-import fruit from './img_fridge/fruit.png';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import "./Demo.css";
+import logo from "./Waf.png";
+import AddItems from "./AddItems";
+import left_fridge from "./img_fridge/left_fridge.png";
+import mandu from "./img_fridge/mandu.png";
+import eggs from "./img_fridge/eggs.png";
+import dairy from "./img_fridge/dairy.png";
+import right_fridge from "./img_fridge/right_fridge.png";
+import fish from "./img_fridge/fish.png";
+import meat from "./img_fridge/meat.png";
+import veges from "./img_fridge/veges.png";
+import fruit from "./img_fridge/fruit.png";
+import { withRouter } from "react-router-dom";
 
 class Demo extends React.Component {
   constructor(props) {
@@ -36,7 +36,7 @@ class Demo extends React.Component {
   };
 
   goToSignup = () => {
-    this.props.history.push('/signup');
+    this.props.history.push("/signup");
   };
 
   componentDidMount() {
@@ -48,58 +48,54 @@ class Demo extends React.Component {
 
   render() {
     return (
-      <div>
-        <div id='demo'>
-          {/* 왼쪽 화면 */}
-          <div className='whiteBackground'>
-            <div className='demoUserInfo'>
-              <div className='name__section'>
-                guest님의 냉장고입니다
-                <span className='loggedOut' onClick={this.goToSignup}>
-                  회원가입
-                </span>
-              </div>
+      <>
+        {/* <div id="demo"> */}
+        {/* 왼쪽 화면 */}
+        <div className="whiteBackground">
+          <div className="demoUserInfo">
+            <div className="name__section">
+              guest님의 냉장고입니다
+              <span className="loggedOut" onClick={this.goToSignup}>
+                회원가입
+              </span>
             </div>
-            <div className='content'>
-              <div className='left'>
-                {/* 왼쪽 냉장고 */}
-                <div className='fridge'>
-                  <div className='fridge_leftside'>
-                    <img className='fridge_left' src={left_fridge}></img>
-                    <div className='left_items'>
-                      <img id='mandu' src={mandu}></img>
-                      <img id='eggs' src={eggs}></img>
-                      <img id='dairy' src={dairy}></img>
-                    </div>
-                  </div>
-
-                  <div className='fridge_rightside'>
-                    <img className='fridge_right' src={right_fridge}></img>
-                    <div className='right_items'>
-                      <img id='seafood' src={fish}></img>
-                      <img id='meat' src={meat}></img>
-                      <div className='freshSection'>
-                        <img id='fruits' src={fruit}></img>
-                        <img id='veges' src={veges}></img>
-                      </div>
-                    </div>
-                  </div>
+          </div>
+          <div className="content">
+            <div className="left">
+              {/* 왼쪽 냉장고 */}
+              <div className="fridge_leftside">
+                <div className="left_items">
+                  <img id="mandu" src={mandu}></img>
+                  <img id="eggs" src={eggs}></img>
+                  <img id="dairy" src={dairy}></img>
                 </div>
               </div>
 
-              {/* 오른쪽 화면*/}
-              <div className='right'>
-                <AddItems
-                  changeItems={this.changeItems}
-                  changePart={this.changePart}
-                  items={this.state.items}
-                  currentPart={this.state.currentPart}
-                />
+              <div className="fridge_rightside">
+                <div className="right_items">
+                  <img id="seafood" src={fish}></img>
+                  <img id="meat" src={meat}></img>
+                  <div className="freshSection">
+                    <img id="fruits" src={fruit}></img>
+                    <img id="veges" src={veges}></img>
+                  </div>
+                </div>
               </div>
+            </div>
+
+            {/* 오른쪽 화면*/}
+            <div className="right">
+              <AddItems
+                changeItems={this.changeItems}
+                changePart={this.changePart}
+                items={this.state.items}
+                currentPart={this.state.currentPart}
+              />
             </div>
           </div>
         </div>
-      </div>
+        {/* </div> */}
+      </>
     );
   }
 }
